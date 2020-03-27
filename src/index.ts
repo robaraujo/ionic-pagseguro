8f5b93dc589a11601e71003dcfa5e82dde74a57b
Roberto Araujo <robaraujo404@gmail.com>
2020-03-26 23:22:40 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagSeguroComponent } from './pagseguro.component';
 import { PagSeguroService } from './pagseguro.service';
-import { IonicModule } from 'ionic-angular';
+import { IonicModule } from '@ionic/angular';
 import ptBr from '@angular/common/locales/pt';
 import ptBrExtra from '@angular/common/locales/extra/pt';
 import { registerLocaleData } from '@angular/common';
 import { MyDatePickerModule } from 'mydatepicker';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 registerLocaleData(ptBr, ptBrExtra);
 
 export * from './pagseguro.component'; 
@@ -18,7 +22,12 @@ export * from './pagseguro.data';
   imports: [
     CommonModule,
     IonicModule,
-    MyDatePickerModule
+    MyDatePickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    HttpClientModule,
+    BrowserModule
   ],
   declarations: [
     PagSeguroComponent
